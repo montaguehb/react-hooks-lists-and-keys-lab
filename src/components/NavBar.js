87eafbf1c8efd5652ec44b7c2df28e57a7fa83a3
Historia Montague <historia.montague@outlook.com>
2023-05-01 18:58:13 -0600

    Finished Lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
-
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const aLinks = links.map(link => <a href={"#" + link} key={link}>{link}</a>)
+  return <nav>{aLinks}</nav>;
 }
 
 export default NavBar;
